dc7685844563e5fcad0d0c506c9cdf49dc7c6dd0
Nesrin Vural Gedik <nesrinvural@gmail.com>
2024-04-10 12:49:04 -0400

    First commit for Family Cash Card App

---
+++ src/test/java/com/cashcard/CashcardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cashcard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CashcardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
