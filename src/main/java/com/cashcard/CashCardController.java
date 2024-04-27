2ce8ea64d30a5a736c7974518bdabff23cb5bda3
Nesrin Vural Gedik <44590816+nesrinvuralgedik@users.noreply.github.com>
2024-04-27 16:30:45 -0400

    Revert "Added a GET endpoint for a list by implementing pagination and sorting with default behavior"
    This reverts commit 93c0fde92bd12fb2a44600c8d20f3455f8f0487f.

--- src/main/java/com/cashcard/CashCardController.java
+++ src/main/java/com/cashcard/CashCardController.java
@@ -1,9 +1,5 @@
 package com.cashcard;
 
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,8 +8,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.util.UriComponentsBuilder;
+
 import java.net.URI;
-import java.util.List;
 import java.util.Optional;
 
 @RestController
@@ -41,17 +37,5 @@ class CashCardController {
         URI locationOfNewCashCard = ucb.path("/cashcards/{id}").buildAndExpand(savedCashCard.id()).toUri();
 
         return ResponseEntity.created(locationOfNewCashCard).build();
-    }
-
-    @GetMapping
-    private ResponseEntity<List<CashCard>> findAll(Pageable pageable){
-        Page<CashCard> page = cashCardRepository.findAll(
-                PageRequest.of(
-                    pageable.getPageNumber(),
-                        pageable.getPageSize(),
-                        pageable.getSortOr(Sort.by(Sort.Direction.ASC, "amount"))
-        ));
-
-        return ResponseEntity.ok(page.getContent());
     }
 }
--- src/main/java/com/cashcard/CashCardRepository.java
+++ src/main/java/com/cashcard/CashCardRepository.java
@@ -1,8 +1,6 @@
 package com.cashcard;
 
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.repository.PagingAndSortingRepository;
 
-interface CashCardRepository extends CrudRepository<CashCard, Long>,
-        PagingAndSortingRepository<CashCard, Long> {
+interface CashCardRepository extends CrudRepository<CashCard, Long> {
 }
